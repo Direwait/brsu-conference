8ef95ddabedbc4bdd4e754189e01b988f5e2859d
Nikita <boreyko1901@mail.ru>
2026-04-08 15:43:05 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,12 +2,17 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
+    host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
-    port: process.env.DB_PORT,
-    host: process.env.HOST
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 1000,        // Самое безопасное значение для бесплатного хостинга
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0
 });
 
 db.connect(err => {
